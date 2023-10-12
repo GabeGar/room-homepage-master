074d7c9c00629efb462ac329344fb35306441876
Gabegar <gagarcia190@gmail.com>
2023-10-12 14:21:51 -0400

    init project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'primary-dark-gray': 'hsl(var(--dark-gray) / <alpha-value>)',
+                'primary-black': 'hsl(var(--black) / <alpha-value>)',
+                'primary-white': 'hsl(var(--white) / <alpha-value>)',
+                'primary-very-dark-gray':
+                    'hsl(var(--very-dark-gray) / <alpha-value>)',
+            },
+            backgroundImage: {
+                'hero-desktop-one':
+                    'url("src/assets/images/desktop-image-hero-1.jpg")',
+                'hero-desktop-two':
+                    'url("src/assets/images/desktop-image-hero-2.jpg")',
+                'hero-desktop-three':
+                    'url("src/assets/images/desktop-image-hero-3.jpg")',
+                'hero-mobile-one':
+                    'url("src/assets/images/mobile-image-hero-1.jpg")',
+                'hero-mobile-two':
+                    'url("src/assets/images/mobile-image-hero-2.jpg")',
+                'hero-mobile-three':
+                    'url("src/assets/images/mobile-image-hero-3.jpg")',
+                'about-dark': 'url("src/assets/images/image-about-dark.jpg")',
+                'about-light': 'url("src/assets/images/image-about-light.jpg")',
+            },
+        },
+    },
+    plugins: [],
+} satisfies Config;
